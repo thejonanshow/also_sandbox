24ae7c583d3d386c2834a65f6e1027a59867f450
Jonan Scheffler <jonanscheffler@gmail.com>
2012-06-06 11:23:48 -0400

    Rails Attack

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlsoSandbox::Application.config.secret_token = '93e29a834a3968becfc109d237a809bff41d3f0b2519c88d3bcf4e0d1c9095bcbed95f5937164a4f0e65097cf0dfb55df5b86c0441de8c9620f8663253f99e55'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AlsoSandbox::Application.config.session_store :cookie_store, key: '_also_sandbox_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AlsoSandbox::Application.config.session_store :active_record_store
